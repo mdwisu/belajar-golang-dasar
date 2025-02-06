a55716839914c1a96cd4147a16467fe22a131ea2
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2025-02-06 10:07:09 +0700

    ✨ learn: variable

---
+++ variable.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	name = "Muhammad Dwi Susanto"
+	fmt.Println(name)
+
+	saya := "dwi"
+	fmt.Println(saya)
+
+	var (
+		firstName = "Muhammad"
+		lastName  = "Dwi"
+	)
+	fmt.Println(firstName, lastName)
+}
