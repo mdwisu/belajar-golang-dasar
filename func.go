99f1b5df6eb5e2174d01ae710fe9aaa9d92e0a4c
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2025-02-06 17:02:07 +0700

    ✨ learn: for break, continue, function

---
+++ func.go
@@ -0,0 +1,19 @@
+package main
+
+func sayhello(firstname string, lastname string) string {
+	println("Hello " + firstname + " " + lastname)
+	return "Hello " + firstname + " " + lastname
+}
+
+// multiple return value
+func getFullName() (string, string) {
+	return "muhammad", "dwi"
+}
+
+func main() {
+	returnvalue := sayhello("Muhammad", "Dwi Susanto")
+	println(returnvalue)
+
+	firstname, lastname := getFullName()
+	println(firstname, lastname)
+}
