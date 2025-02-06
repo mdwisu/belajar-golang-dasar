e7aadeb005e980ca1fb10c9e8c15ec2ab358d823
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2025-02-06 10:10:07 +0700

    ✨ learn: constant

---
+++ constant.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	const name string = "Muhammad Dwi Susanto"
+	const age int = 20
+
+	fmt.Println(name)
+	fmt.Println(age)
+
+	const (
+		firstName = "Muhammad"
+		lastName  = "Dwi"
+	)
+	
+}
