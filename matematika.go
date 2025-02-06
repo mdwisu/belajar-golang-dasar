a29adf324450122c67784f20fa2a4be5ae242fe8
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2025-02-06 10:24:35 +0700

    ✨ learn: operation mathematic

---
+++ matematika.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 20
+	b := 20
+	c := a + b
+	d := a - b
+	e := a * b
+	f := a / b
+	
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
+}
